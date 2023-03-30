84d0b727f0ccfc121a5911a24ba8930c5b14113f
Ali <Ali@DESKTOP-CLIARJ6>
2023-03-31 01:24:44 +0330

    Discount.API init

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(
+    c => c.SwaggerDoc("v1", new OpenApiInfo() { Title = "Discount.API", Version = "v1" })
+
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
