77ff25c1696dd3e5834eab142e48d25337897df3
Ali <Ali@DESKTOP-CLIARJ6>
2023-04-01 18:28:05 +0330

    Discount.API done create Dockerfile add DiscountApi to docker compose

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,8 +1,14 @@
+using Discount.API.Repositories.Abstraction;
+using Discount.API.Repositories.Implemention;
 using Microsoft.OpenApi.Models;
+using Discount.API.Extensions;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,7 +19,7 @@ builder.Services.AddSwaggerGen(
 );
 
 var app = builder.Build();
-
+app.MigrateDatabase<Program>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
