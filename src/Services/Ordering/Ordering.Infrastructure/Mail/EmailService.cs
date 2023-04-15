78fa5a31ad560bcf3aff3b873eec5415136d7b68
Ali <Ali@DESKTOP-CLIARJ6>
2023-04-15 23:29:01 +0330

    add repo base add order repository add email service

---
+++ src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastracture;
+using Ordering.Application.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Ordering.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSettings _emailSettings { get; }
+        public ILogger<EmailService> _logger { get; }
+
+        public EmailService(EmailSettings emailSettings, ILogger<EmailService> logger)
+        {
+            _emailSettings = emailSettings ?? throw new ArgumentNullException(nameof(emailSettings));
+            _logger = logger ?? throw new ArgumentNullException(nameof(_logger));
+        }
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(_emailSettings.ApiKey);
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+
+            var from = new EmailAddress()
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName
+            };
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            _logger.LogInformation("Email sent !");
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+                return true;
+
+            _logger.LogInformation("Email Sending Failed !");
+            return false;
+        }
+    }
+}
